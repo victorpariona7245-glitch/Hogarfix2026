4b5ce9719c0848cce824f36034197d75a92b58e5
victorpariona7245-glitch <victorpariona7245@gmail.com>
2026-09-23 03:20:47 -0500

    Agrega templates, static/images, tests, corrige logback-spring.xml y protege credenciales en application.properties

---
+++ src/test/java/com/hogarfix/HogarFixApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hogarfix;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HogarFixApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
